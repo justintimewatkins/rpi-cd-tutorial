5a6c9e23f20b8b64af435861c68d517702c4dd48
Justin Watkins <justin_watkins@uri.edu>
2025-06-12 16:40:44 +0100

    this sucks

--- src/app.py
+++ src/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Welcome to the Raspberry Pi web application! Hello World!!!'
+  return 'Welcome to the Raspberry Pi web application! HELLO Hello World!!!'
